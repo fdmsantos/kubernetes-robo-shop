17867c5ae64b0326d9b4325e1bec87e7b8fe1ced
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2022-06-25 12:02:24 +0100

    Fix external dns

--- helpers/deploy.sh
+++ helpers/deploy.sh
@@ -4,7 +4,7 @@ terraform init
 AWS_PROFILE=outscope-tests terraform apply --auto-approve
 # Configure Kubectl
 aws eks update-kubeconfig --region eu-west-1 --name robotshop-cluster --profile outscope-tests
-kubectl apply -f https://s3.us-west-2.amazonaws.com/amazon-eks/docs/eks-console-full-access.yaml
+kubectl apply -f https://s3.us-west-2.amazonaws.com/amazon-eks/docs/eks-console-full-access.yaml # Migrate to gitops process
 # Deploy Flux Components (GitOps)
 kubectl apply -f flux-system/gotk-components.yaml
 kubectl apply -f flux-system/gotk-sync.yaml
