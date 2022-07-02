d556c2a9980f558cbb60613dd2202fa59ab4eea1
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2022-07-02 14:40:22 +0100

    Add cluster autoscaler controller

--- clusters/main.tf
+++ clusters/main.tf
@@ -143,6 +143,19 @@ module "external_dns_route53_role" {
   }
 }
 
+module "cluster_autoscaler_role" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name                     = "AmazonEKSClusterAutoscalerRole"
+  attach_cluster_autoscaler_policy = true
+  cluster_autoscaler_cluster_ids = [module.eks.cluster_id]
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cluster-autoscaler"]
+    }
+  }
+}
+
 module "acm" {
   source              = "terraform-aws-modules/acm/aws"
   version             = "~> 3.0"
