6d7f8fad0915c0aeeafd58ca3f0e93bcb4bb94a1
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2022-07-02 15:08:22 +0100

    Improve destroy script

--- helpers/destroy.sh
+++ helpers/destroy.sh
@@ -1,7 +1,9 @@
 flux delete -s kustomization apps
+flux delete -s kustomization ingress-infrastructure
 flux delete -s kustomization istio-infrastructure
 flux delete -s kustomization aws-elb-controller
 flux delete -s kustomization external-dns-route53
+flux delete -s kustomization cluster-autoscaler-controller
 flux delete -s kustomization istio-system
 cd clusters
 AWS_PROFILE=outscope-tests terraform destroy --auto-approve
