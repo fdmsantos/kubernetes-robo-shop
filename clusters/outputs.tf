5e0a2ec946484682d4385ff4eb4fda29b1fe776b
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2022-07-08 16:52:08 +0100

    replace elasticsearch port

--- clusters/outputs.tf
+++ clusters/outputs.tf
@@ -1,3 +1,7 @@
 output "elasticsearch_dashboards_url" {
   value = var.enable_elasticsearch ? "http://${module.es-proxy[0].public_ip}" : null
 }
+
+output "elasticsearch_domain_name" {
+  value = var.enable_elasticsearch ? module.elasticsearch[0].domain_hostname : null
+}
